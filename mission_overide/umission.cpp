f303d841e54025f73c9c3503b887104a138ac371
DJacquemont <101003659+DJacquemont@users.noreply.github.com>
2022-03-11 16:44:02 +0100

    Update umission.cpp

--- mission_overide/umission.cpp
+++ mission_overide/umission.cpp
@@ -386,8 +386,16 @@ bool UMission::mission1(int & state)
       snprintf(lines[0], MAX_LEN, "vel=0.75, edger = 0 : ir1<0.3");
       snprintf(lines[1], MAX_LEN, "vel=0.75, edger = 0 : dist = 0.5");
       snprintf(lines[2], MAX_LEN, "vel=0.75, edger = 0 : ir1<0.3");
-      snprintf(lines[3], MAX_LEN, "vel=0.5, edger = 0 : dist = 0.3");
-      snprintf(lines[3], MAX_LEN, "vel = 0: time = 0.5");
+      
+      snprintf(lines[3], MAX_LEN, "vel=0.5,edgel=0:xl>15");
+      snprintf(lines[4], MAX_LEN, "vel=0.5 , tr=0.15: turn=90");
+      snprintf(lines[5], MAX_LEN, "vel=0.5, edger = 0 : dist = 0.3");
+      snprintf(lines[6], MAX_LEN, "vel=0.5, edger = 0 : lv<10");
+      snprintf(lines[7], MAX_LEN, "vel=0.5:dist=0.75");
+      snprintf(lines[8], MAX_LEN, "vel=0.3, tr=0: turn=-90");
+      snprintf(lines[9], MAX_LEN, "vel=0.75 : lv=1");
+      
+      snprintf(lines[10], MAX_LEN, "vel = 0: time = 0.2");
       // send the 4 lines to the REGBOT
       sendAndActivateSnippet(lines, 5);
       // make sure event 1 is cleared
