7851be8401be814b3cbb8f7d8ded0efc1d68e87c
DJacquemont <101003659+DJacquemont@users.noreply.github.com>
2022-03-31 12:39:51 +0200

    Update umission.cpp

--- mission_overide/umission.cpp
+++ mission_overide/umission.cpp
@@ -403,11 +403,11 @@ bool UMission::mission1(int & state)
     case 10:
       int line = 0;
       snprintf(lines[line++], MAX_LEN, "servo=3, pservo=0, vservo=0");
-      snprintf(lines[line++], MAX_LEN, "vel=0.4, edgel = 0 : dist = 0.3");
-      snprintf(lines[line++], MAX_LEN, "vel=0.4, edgel = 0 : ir1<0.3");
-      snprintf(lines[line++], MAX_LEN, "vel=0.4, edgel = 1 : dist = 0.5");
+      snprintf(lines[line++], MAX_LEN, "vel=0.3, edgel = 0 : dist = 0.3");
+      snprintf(lines[line++], MAX_LEN, "vel=0.5, edgel = 0 : ir1<0.3");
+      snprintf(lines[line++], MAX_LEN, "vel=0.5, edgel = 1 : dist = 0.5");
       snprintf(lines[line++], MAX_LEN, "vel=0.4, edgel = 1 : ir1<0.3");
-      snprintf(lines[line++], MAX_LEN, "vel=0.1, edgel = 2 : xl>10");
+      snprintf(lines[line++], MAX_LEN, "vel=0.1, edgel = 1 : xl>15");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, tr=0.15: turn=90.0");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger=0.0: dist=0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger=0.0: lv<10");
