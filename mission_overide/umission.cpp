3288fec8cd5f942f6cc996396869dbd5de57d1a7
DJacquemont <101003659+DJacquemont@users.noreply.github.com>
2022-03-31 13:09:02 +0200

    Update umission.cpp

--- mission_overide/umission.cpp
+++ mission_overide/umission.cpp
@@ -405,9 +405,10 @@ bool UMission::mission1(int & state)
       snprintf(lines[line++], MAX_LEN, "servo=3, pservo=0, vservo=0");
       snprintf(lines[line++], MAX_LEN, "vel=0.4, edger = 0 : dist = 0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.75, edger = 0 : ir1<0.3");
-      snprintf(lines[line++], MAX_LEN, "vel=0.5, edger = 1 : dist = 0.5");
-      snprintf(lines[line++], MAX_LEN, "vel=0.5, edger = 1 : ir1<0.3");
-      snprintf(lines[line++], MAX_LEN, "vel=0.1, edger = 2 : xl>5");
+      snprintf(lines[line++], MAX_LEN, "vel=0.5, edger = 0 : dist = 2");
+      snprintf(lines[line++], MAX_LEN, "vel=0.5, edger = 2 : ir1<0.3");
+      snprintf(lines[line++], MAX_LEN, "vel=0 : time = 1");
+      snprintf(lines[line++], MAX_LEN, "vel=0.1, edger = 2 : xl>10");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, tr=0.15: turn=90.0");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger=0.0: dist=0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger=0.0: lv<10");
@@ -421,7 +422,7 @@ bool UMission::mission1(int & state)
       
       snprintf(lines[line++], MAX_LEN, "event=1, vel=0");
       snprintf(lines[line++], MAX_LEN, ": dist=1");
-      sendAndActivateSnippet(lines, 15);
+      sendAndActivateSnippet(lines, line);
       
      
       // send the 4 lines to the REGBOT
