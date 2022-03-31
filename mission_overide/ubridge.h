c7d862c383feb3412301f7c5b7283e22ad8d13a2
DJacquemont <101003659+DJacquemont@users.noreply.github.com>
2022-03-31 15:39:03 +0200

    Update ubridge.h

--- mission_overide/ubridge.h
+++ mission_overide/ubridge.h
@@ -265,6 +265,7 @@ public:
   /**
    * Clear all event flags to false */
   void clearEvents();
+  void clearEvent(int i)
   /**
    * Requests if this event has occured.
    * \param event the event flag to test
