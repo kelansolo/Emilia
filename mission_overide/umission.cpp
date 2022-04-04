941d81995f07b0eb58031fab11c579b5402639b6
DJacquemont <101003659+DJacquemont@users.noreply.github.com>
2022-04-04 15:48:33 +0200

    Update umission.cpp

--- mission_overide/umission.cpp
+++ mission_overide/umission.cpp
@@ -408,8 +408,10 @@ bool UMission::mission1(int & state)
       snprintf(lines[line++], MAX_LEN, "vel=0.4, edger = 0 : dist=0.7");
       //snprintf(lines[line++], MAX_LEN, "vel=0.3, edger = 0 : dist = 0.3");
       //snprintf(lines[line++], MAX_LEN, "vel=0.2, edger = 0 : xl>15");
+      snprintf(lines[line++], MAX_LEN, "servo=3, pservo=800, vservo=0:time=0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.3, tr=0.15: turn=90.0");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger=0.0: dist=0.3");
+      snprintf(lines[line++], MAX_LEN, "servo=3, pservo=-50, vservo=0:time=0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger=0.0: ir1<0.5");
       snprintf(lines[line++], MAX_LEN, "servo=3, pservo=800, vservo=0:time=0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.5: dist=0.75");
