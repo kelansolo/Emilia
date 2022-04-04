1f3b51ac1115f09f603c2c20c1f3999f5ecdfc7d
DJacquemont <101003659+DJacquemont@users.noreply.github.com>
2022-04-04 16:14:30 +0200

    Update umission.cpp

--- mission_overide/umission.cpp
+++ mission_overide/umission.cpp
@@ -400,25 +400,22 @@ bool UMission::mission1(int & state)
     case 10:
       int line = 0;
       snprintf(lines[line++], MAX_LEN, "servo=3, pservo=0, vservo=0");
-      snprintf(lines[line++], MAX_LEN, "vel=0.1, edger = 0 : time = 0.3");
+      snprintf(lines[line++], MAX_LEN, "vel=0.1, acc=3, edger = 0 : time = 0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.4, edger = 0 : dist = 0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.75, edger = 0 : ir1<0.5");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger = 0 : dist = 2");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger = 0 : ir1<0.5");
       snprintf(lines[line++], MAX_LEN, "vel=0.4, edger = 0 : dist=0.7");
-      //snprintf(lines[line++], MAX_LEN, "vel=0.3, edger = 0 : dist = 0.3");
-      //snprintf(lines[line++], MAX_LEN, "vel=0.2, edger = 0 : xl>15");
       snprintf(lines[line++], MAX_LEN, "servo=3, pservo=800, vservo=0:time=0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.3, tr=0.15: turn=90.0");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger=0.0: dist=0.3");
-      snprintf(lines[line++], MAX_LEN, "servo=3, pservo=-50, vservo=0:time=0.3");
       snprintf(lines[line++], MAX_LEN, "vel=0.5, edger=0.0: ir1<0.5");
-      snprintf(lines[line++], MAX_LEN, "servo=3, pservo=800, vservo=0:time=0.3");
-      snprintf(lines[line++], MAX_LEN, "vel=0.5: dist=0.75");
+      snprintf(lines[line++], MAX_LEN, "vel=0.5: dist=0.70");
       snprintf(lines[line++], MAX_LEN, "vel=0.3, tr=0.0: turn=-90.0");
       snprintf(lines[line++], MAX_LEN, "servo=3, pservo=-50, vservo=0");
       snprintf(lines[line++], MAX_LEN, "vel=0.3: xl>15");
-      snprintf(lines[line++], MAX_LEN, "vel=0.3, tr=0.05: turn=90.0");
+      snprintf(lines[line++], MAX_LEN, "vel=0.3: dist=0.1");
+      snprintf(lines[line++], MAX_LEN, "vel=0.3, tr=0: turn=90.0");
       snprintf(lines[line++], MAX_LEN, "vel=0.3, edgel=1.0: lv<5");
       
       snprintf(lines[line++], MAX_LEN, "event=1, vel=0: dist=1");
