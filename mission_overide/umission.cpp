b452aef96a3c57c43ae4c10fdec930d93cabb25f
carikaser <100944291+carikaser@users.noreply.github.com>
2022-04-06 13:18:26 +0200

    Update umission.cpp

--- mission_overide/umission.cpp
+++ mission_overide/umission.cpp
@@ -261,11 +261,11 @@ void UMission::runMission()
         }
         switch(mission)
         {
-         
+         /*
           case 1: // running auto mission
             ended = mission1(missionState);
             break;
-            /*
+            
           case 2:
             ended = mission2(missionState);
             break;
